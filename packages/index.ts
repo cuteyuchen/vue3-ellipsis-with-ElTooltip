d793a50f6e46317c03291a296987df08fca3c62a
cuteyuchen <53141871+cuteyuchen@users.noreply.github.com>
2024-03-25 17:01:25 +0800

    Add files via upload
    init

---
+++ packages/index.ts
@@ -0,0 +1,10 @@
+import vue3EllipsisWithElTooltip from "./vue3-ellipsis-with-ElTooltip.vue"
+
+
+vue3EllipsisWithElTooltip.name = 'vue3EllipsisWithElTooltip'
+
+vue3EllipsisWithElTooltip.install = function(Vue: any) {
+  // 注册组件
+  Vue.component('vue3EllipsisWithElTooltip', vue3EllipsisWithElTooltip)
+}
+export default vue3EllipsisWithElTooltip
---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import path from "path";
+
+const resolve = (dir: any) => path.join(__dirname, dir);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve("examples"),
+      packages: resolve("packages")
+    }
+  },
+  build: {
+    cssCodeSplit: true,
+    rollupOptions: {
+      // 请确保外部化那些你的库中不需要的依赖
+      external: ['vue', 'element-plus'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    },
+    lib: {
+      entry: 'packages/index.ts',
+      name: 'vue3-ellipsis-with-ElTooltip',
+      fileName: (format) => `vue3-ellipsis-with-ElTooltip.${format}.js`
+    }
+  }
+})
